1e801166b03698305b654381f202244648d14f51
Sara Nolen <nolensara1@gmail.com>
2020-12-07 19:49:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
+def my_collect(empty_array)
+  i = 0
+  collection = []
+while i< empty_array.length
+  collection <<  yield(empty_array[i])
+  i += 1
+end
+collection
+end
 
-
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect([languages]){|language| "#{language}"}
